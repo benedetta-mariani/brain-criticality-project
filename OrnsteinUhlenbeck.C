0772333e815bf413419d5a93327f6744b5565001
benedetta-mariani <benedetta.mariani@edu.unito.it>
2020-09-03 14:42:12 +0200

    update

--- OrnsteinUhlenbeck.C
+++ OrnsteinUhlenbeck.C
@@ -73,27 +73,21 @@ double* OrnsteinUhlebeck(double dt, double T, double tau, double* D) {
 }
 
 double* OUGillespieDiffusion(double dt, double T, double tau, double D){
-
-
 	int N = int(T/dt);
 	int n = int(tau/dt*100) + N; // to avoid initial transient
-	double *x = new double[n];	
-
+	double *x = new double[n];
 	x[0] = 0;
     double mu = TMath::Exp(-dt/tau);
     double sigma = TMath::Sqrt(D*tau/2*(1-pow(mu,2)));
-
     for (int t = 0; t < n-1; t ++){
         x[t+1] = x[t]*mu + sigma*gRandom->Gaus(0,1);
      }
-  
 	for (int i = 0; i < n; i ++){
 		if (x[i] < 0.2){
 			x[i] = 0.2;
 		}
-	}
-	
-return x;
+	}	
+	return x;
 
 }
 
